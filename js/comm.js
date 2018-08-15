7f3fa82874f9381c4f52665d57ebb08efe952207
cc <chenorange12@gmail.com>
2018-08-15 20:16:10 +0800

    js selectlist

---
+++ js/comm.js
@@ -0,0 +1,18 @@
+window.onload=function(){
+			var navh=document.getElementById("navh");
+			var startlist=document.getElementById("startlist");
+			navh.onclick=function(){
+				var style=startlist.style;
+				console.log(style.display);
+		// style.display = style.display == "block" ? "none" : "block";
+		// navh.className = style.display == "block" ? "open" : ""
+
+			if(style.display=="block"){
+				style.display = "none";
+				navh.className=" ";
+			}else{
+				style.display = "block";
+				navh.className="open";
+			}
+		}
+}
